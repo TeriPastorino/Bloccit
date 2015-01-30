d01d5a8f37e369655a843a70af6a4513e6894e83
Teri Pastorino <tpasto@comcast.net>
2015-01-30 14:36:24 -0800

    added index and show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
-Rails.application.routes.draw do
-  get 'welcome/index'
-  get 'welcome/about'
-  get 'welcome/contact'
+Bloccit::Application.routes.draw do
 
-    root to: 'welcome#index'
+  resources :posts
+
+  get 'about' => 'welcome#about'
+
+  root to: 'welcome#index'
 
   end
