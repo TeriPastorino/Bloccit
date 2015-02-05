7c1c6e46bc30b680067c3f45f6b41ef91e2c33d0
Teri Pastorino <tpasto@comcast.net>
2015-02-05 12:21:50 -0800

    deploy to heroku and refactor

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Bloccit::Application.routes.draw do
   get 'topics/edit'
 
   devise_for :users
+    resources :users, only: [:update]
 
     resources :topics do
       resources :posts, except: [:index]
