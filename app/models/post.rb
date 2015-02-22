2116ec41c8cdafeea3c0025a44c3ff3cf17999df
Teri Pastorino <tpasto@comcast.net>
2015-02-22 08:18:59 -0800

    error vote controller to end of exerciser

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,12 +4,15 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :topic
 
-  default_scope { order('created_at DESC')}
+  default_scope { order('rank DESC')}
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   #validates :topic, presence: true
   #rvalidates :user, presence: true
+
+  after_create :create_vote
+  
   
  mount_uploader :image, ImageUploader
 
@@ -26,6 +29,13 @@ class Post < ActiveRecord::Base
   def points
     votes.sum(:value).to_i
   end
+
+  def update_rank
+    age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24)
+      new_rank = points + age_in_days
+
+      update_attribute(:rank, new_rank)
+  end
  
   def markdown_title
     render_as_markdown(self.title)
@@ -41,6 +51,10 @@ class Post < ActiveRecord::Base
     extensions = {fenced_code_blocks: true, highlight: true}
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render markdown).html_safe
+  end
+
+  def create_vote
+    user.votes.create(value: 1, post: self)
   end
 
 end
