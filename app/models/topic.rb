781c23235fda5069cc64b363e18f5065496b7eef
Teri Pastorino <tpasto@comcast.net>
2015-02-24 17:30:52 -0800

    complete w/uninitialized error

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,10 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+#ruby guide scopes
+
+    scope :publicly_viewable, -> { where(public: true) }
+    scope :privately_viewable, -> {where(public: false) }
+    scope :visible_to, -> (user) { user ? all : publicly_viewable }
+
 
 end
